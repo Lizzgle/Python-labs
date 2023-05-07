10ead3d957fb642241efa9e14065937fe762de0a
lizzgle <lizaglebtsova@gmail.com>
2023-05-08 02:28:25 +0300

    add some abbreviions

---
+++ lab2/task1/constants.py
@@ -0,0 +1,7 @@
+ABBREVIATIONS = (
+    r'Mr\.', r'Mrs\.', r'Dr\.', r'Lt\.', r'Rep\.', r'B\.A\.', r'Ph\.D\.',
+    r'Jan\.', r'Feb\.', r'Mar\.', r'Apr\.', r'Jn\.', r'Jl\.', r'Aug\.', r'Sep\.', r'Oct\.', r'Now\.', r'Dec\.',
+    r'Mon\.', r'Tue\.', r'Wed\.', r'Th\.', r'Fr\.', r'Sat\.', r'Sn\.', r'p\.m\.',
+    r'ft\.', r'cm\.', r'kg\.', r'g\.',  r'lbs\.', r'in\.', r'sec\.',
+    r'etc\.', r'e\.g\.', r'c\.', r'i\.e\.', r'exp\.', r'ex\.', r'vs\.', r'err\.', r'P\.S\.', r'P\.S\.S\.'
+)
