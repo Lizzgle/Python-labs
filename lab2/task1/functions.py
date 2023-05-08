81ac92097c45f2d9012c868b2bc6b8e8231e5434
lizzgle <lizaglebtsova@gmail.com>
2023-05-08 18:42:26 +0300

    add counting of average length of the sentence and of the word

--- lab2/task1/functions.py
+++ lab2/task1/functions.py
@@ -2,19 +2,19 @@ import re
 from task1.constants import ABBREVIATIONS, FILES, ELLIPSIS, DOUBLE_SIGNS, NAME, NUMBERS, MATH_SIGNS
 from task1.constants import NUM_SENTENCES, NUM_NONDECLARE_SENTENCES, PUNCTUATION
 def correctText(text):
-    word = re.sub(FILES, "", text)
-    word = re.sub(ELLIPSIS, ".", word)
-    word = re.sub(DOUBLE_SIGNS, "?", word)
-    word = re.sub(NAME, "", word)
-    word = re.sub(NUMBERS, "", word)
-    word = re.sub(MATH_SIGNS, "", word)
+    t = re.sub(FILES, "", text)
+    t = re.sub(ELLIPSIS, ".", t)
+    t = re.sub(DOUBLE_SIGNS, "?", t)
+    t = re.sub(NAME, "", t)
+    t = re.sub(NUMBERS, "", t)
+    t = re.sub(MATH_SIGNS, "", t)
 
     for abbr in ABBREVIATIONS:
-        word = re.sub(abbr, "", word)
+        t = re.sub(abbr, "", t)
 
-    print(word)
+    # print(t)
 
-    return word
+    return t
 
 def amountOfSentences(text):
     return len(re.findall(NUM_SENTENCES, text))
@@ -27,7 +27,28 @@ def showOnlyWords(text):
     t = re.sub(MATH_SIGNS, "", t)
     t = re.sub(PUNCTUATION, "", t)
 
-    print(t)
+    # print(t)
 
     return t.split()
 
+def lengthAllWords(onlyWords):
+    lengthAllWords = 0
+
+    for word in onlyWords:
+        lengthAllWords += len(word)
+
+    # print(lengthAllWords)
+    return lengthAllWords
+
+def averageLengthSent(correctText, onlyWords):
+    countSent = amountOfSentences(correctText)
+
+    if not countSent:
+        return 0
+
+    # print(countSent)
+    return round(lengthAllWords(onlyWords) / countSent)
+
+def averageLengthWords(onlyWords):
+    # print(len(onlyWords))
+    return round(lengthAllWords(onlyWords) / len(onlyWords))
--- lab2/task1/mainTask1.py
+++ lab2/task1/mainTask1.py
@@ -1,14 +1,17 @@
-from task1.functions import correctText, amountOfSentences, amountOfNondecSentences, showOnlyWords
+from task1.functions import correctText, amountOfSentences, amountOfNondecSentences, showOnlyWords, averageLengthSent, averageLengthWords
 def mainTask1():
     with open("task1/test.txt") as f:
         text = f.read()
     print(text)
 
     CORRECT_TEXT = correctText(text)
+    ONLY_WORDS = showOnlyWords(text)
 
     print(f"amount of sentences in the text: {amountOfSentences(CORRECT_TEXT)}")
 
     print(f"amount of non-declarative sentences in the text: {amountOfNondecSentences(CORRECT_TEXT)}")
 
-    print(f"{showOnlyWords(text)}")
-    # print(f"average length of the sentence in characters (words count only): ")
+    # print(f"{showOnlyWords(text)}")
+    print(f"average length of the sentence in characters: {averageLengthSent(CORRECT_TEXT, ONLY_WORDS)}")
+
+    print(f"average length of the word in the text in characters: {averageLengthWords(ONLY_WORDS)}")
