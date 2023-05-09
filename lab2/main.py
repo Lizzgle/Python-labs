2b08c325775dc13bc8c332a48957388a84c48700
lizzgle <lizaglebtsova@gmail.com>
2023-05-09 17:17:19 +0300

    add main for task2 and update main.py

--- lab2/main.py
+++ lab2/main.py
@@ -1,8 +1,10 @@
 from task1.mainTask1 import mainTask1
+from task2.mainTask2 import mainTask2
 
 
 def main():
-    mainTask1()
+    # mainTask1()
+    mainTask2()
 
 if __name__ == '__main__':
     main()
---
+++ lab2/task2/mainTask2.py
@@ -0,0 +1,2 @@
+def mainTask2():
+    pass
