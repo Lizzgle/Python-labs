6d771c5c0b95154b323c2486b1303db93621be3a
lizzgle <lizaglebtsova@gmail.com>
2023-05-10 02:37:11 +0300

    add function grep

--- lab2/task2/container.py
+++ lab2/task2/container.py
@@ -1,4 +1,4 @@
-
+import re
 class Container:
 
     def __int__(self, *args):
@@ -32,3 +32,17 @@ class Container:
     def list(self):
         for elem in self.__container:
             print(elem)
+
+    def grep(self, regex: str):
+        result = list()
+        try:
+            for elem in self.__container:
+                if re.fullmatch(regex, elem.__str__()):
+                    result.append(elem.__str__())
+        except Exception:
+            return "Error in the regular expression"
+
+        if not result:
+            return "No such element"
+
+        return result
