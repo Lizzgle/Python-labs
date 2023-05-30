ddc97b4331a2fc75a2c254f6f94245127c1660af
Liza <lizaglebtsova@gmail.com>
2023-05-30 06:35:29 +0300

    add serialize base collections and functions

--- lab3/my_serializer/constants.py
+++ lab3/my_serializer/constants.py
@@ -4,3 +4,24 @@ BASE_TYPES = {"str": str, "int": int, "bool": bool, "float": float, "complex": c
 BASE_COLLECTIONS = {"list": list, "tuple": tuple, "frozenset": frozenset, "set": set, "bytes": bytes,
                     "bytearray": bytearray, "dict": dict}
 
+SIMILAR_COLLECTIONS = {"list": list, "tuple": tuple, "frozenset": frozenset, "set": set, "bytes": bytes,
+                       "bytearray": bytearray}
+
+CODE_ATTRIBUTES = ("co_argcount",
+                    "co_posonlyargcount",
+                    "co_kwonlyargcount",
+                    "co_nlocals",
+                    "co_stacksize",
+                    "co_flags",
+                    "co_code",
+                    "co_consts",
+                    "co_names",
+                    "co_varnames",
+                    "co_filename",
+                    "co_name",
+                    #"co_qualname",
+                    "co_firstlineno",
+                    "co_lnotab",
+                    #"co_exceptiontable",
+                    "co_freevars",
+                    "co_cellvars")
--- lab3/my_serializer/serializer.py
+++ lab3/my_serializer/serializer.py
@@ -1,7 +1,8 @@
+import inspect
 import re
 import types
 
-from my_serializer.constants import BASE_TYPES
+from my_serializer.constants import BASE_TYPES, SIMILAR_COLLECTIONS, CODE_ATTRIBUTES
 
 def serialize(obj):
 
@@ -10,6 +11,15 @@ def serialize(obj):
 
     elif isinstance(obj, types.NoneType):
         return serialize_none_type()
+
+    elif isinstance(obj, tuple(SIMILAR_COLLECTIONS.values())):
+        return serialize_similar_collections(obj)
+
+    elif isinstance(obj, dict):
+        return serialize_dict(obj)
+
+    elif inspect.isfunction(obj):
+        return serialize_function(obj)
 
 def get_obj_type(obj):
     return re.search(r"\'(\w+)\'", str(type(obj)))[1]
@@ -27,3 +37,62 @@ def serialize_none_type():
     srz["type"] = "NoneType"
     srz["value"] = "definitely none"
     return srz
+
+def serialize_similar_collections(obj):
+    srz = dict()
+
+    srz["type"] = get_obj_type(obj)
+    srz["value"] = [serialize(item) for item in obj]
+    return srz
+
+def serialize_dict(obj):
+    srz = dict()
+
+    srz["type"] = get_obj_type(obj)
+    srz["value"] = [[serialize(key), serialize(value)] for (key, value) in obj.items()]
+    return srz
+
+def serialize_function(obj):
+    srz = dict()
+    srz["type"] = "function"
+    srz["value"] = full_function_serialize(obj)
+
+    return srz
+
+def full_function_serialize(obj, cls=None):
+    value = dict()
+
+    value["__name__"] = obj.__name__
+    value["__globals__"] = get_globals(obj, cls)
+    value["__closure__"] = serialize(obj.__closure__)
+
+    arguments = {key: serialize(value) for key, value in inspect.getmembers(obj.__code__)
+                 if key in CODE_ATTRIBUTES}
+
+    value["__code__"] = arguments
+
+    return value
+
+
+def get_globals(func, cls=None):
+    glob = dict()
+
+    for glob_var in func.__code__.co_names:
+        if (glob_var in func.__globals__):
+            if (isinstance(func.__globals__[glob_var], types.ModuleType)):
+                glob["module " + glob_var] = serialize(func.__globals__[glob_var].__name__)
+
+            elif (inspect.isclass(func.__globals__[glob_var])):
+                if (cls and func.__globals__[glob_var] != cls) or (not cls):
+                    glob[glob_var] = serialize(func.__globals__[glob_var])
+
+
+            elif (glob_var != func.__code__.co_name):
+                glob[glob_var] = serialize(func.__globals__[glob_var])
+
+            # на случай рекурсии
+            else:
+                glob[glob_var] = serialize(func.__name__)
+
+    return glob
+
